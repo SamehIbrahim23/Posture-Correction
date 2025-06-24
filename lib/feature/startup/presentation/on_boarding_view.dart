ff2bbd4f013955da1f5a69df4398358d9690e3a5
SamehEldagwy <sameh.hazem504@gmail.com>
2025-06-24 22:33:02 +0300

    integrate splash and on boarding screen (rest statistics)

--- lib/feature/startup/presentation/on_boarding_view.dart
+++ lib/feature/startup/presentation/on_boarding_view.dart
@@ -1,7 +1,9 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../core/routing/routes.dart';
 import '../../../core/utils/app_colors.dart';
 import 'widget/on_boarding_page.dart';
 
@@ -28,17 +30,17 @@ class _OnboardingState extends State<Onboarding> {
               onPageChanged: (int page) => setState(() => _currentPage = page),
               children: [
                 OnboardingPage(
-                    image: 'assets/1.jpg',
+                    image: 'assets/images/1.jpg',
                     title: "Welcome to Posture App",
                     description:
                         "Sit smarter, not harder. Our app helps you stay aligned throughout your day with real-time posture feedback."),
                 OnboardingPage(
-                    image: 'assets/2.jpg',
+                    image: 'assets/images/2.jpg',
                     title: "How Does the App Work?",
                     description:
                         "Using your phone and smart AI, we analyze your posture and gently alert you when you start to slouch."),
                 OnboardingPage(
-                    image: 'assets/3.jpg',
+                    image: 'assets/images/3.jpg',
                     title: "Track Your Progress!",
                     description:
                         "View your daily improvements and build healthier habits with posture statistics ."),
@@ -70,10 +72,14 @@ class _OnboardingState extends State<Onboarding> {
                 ),
                 TextButton(
                   onPressed: () {
-                    _pageController.nextPage(
-                      duration: Duration(milliseconds: 300),
-                      curve: Curves.easeIn,
-                    );
+                    if (_currentPage < 2) {
+                      _pageController.nextPage(
+                        duration: Duration(milliseconds: 300),
+                        curve: Curves.easeIn,
+                      );
+                    } else {
+                      context.go(Routes.streaming);
+                    }
                   },
                   child: Text(_currentPage == 2 ? "FINISH" : "NEXT",
                       style: TextStyle(
